c3cc1c04eda35d66da71af560076bc4c1cfe10fb
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2022-06-21 01:34:32 +0530

    Input bot added

--- looping.py
+++ looping.py
@@ -1,18 +1,8 @@
-#while loop
+# input bot
 
-r = input("Enter the range : ")
-i = 1
-while i<= int(r) :
-    print(i)
-    i += 1
+user_input = input(">>> ")
+print("Ok great !", user_input)
 
-print(5*"*")
-
-#extracing the digs
-
-b = 23
-n = 0
-
-while b>0:
-    print(b%10)
-    b = int(b/10)
+while user_input != "exit":
+    user_input = input(">>> ")
+    print("Ok great !", user_input)
