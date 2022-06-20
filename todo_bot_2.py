d177bff669f56ff8e2d4dc9260f2e4a6b2f9f5cc
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2022-06-21 03:08:04 +0530

    ToDo TerminalBot2 Final Ver

--- todo_bot_2.py
+++ todo_bot_2.py
@@ -1,11 +1,12 @@
 user_input = 'random'
+data = []
 
 def show_menu():
     print("")
     print("***  Menu  ***")
     print("1. add an item to todo")
-    print("1. mark as do")
-    print("1. all todo items")
+    print("2. mark as done")
+    print("3. all todo items")
     print("4. exit")
 
 while user_input != "4":
@@ -13,11 +14,21 @@ while user_input != "4":
     user_input = input("Enter your choice : ")
 
     if user_input == "1":
-        print("Add an item")
+        item = input("What is to be done ? ")
+        data.append(item)
+        print(item, "Added ToDo")
     elif user_input == "2":
-        print("Mark as done")
+        item = input("What is done? ")
+        if item in data:
+            data.remove(item)
+            print(item, "Marked as done and removed from ToDo")
+        else:
+            print(item, "is no there in your todo list")
     elif user_input == "3":
-        print("view the todo items")
+        print(" ")
+        print("Your ToDo List :")
+        for task in data:
+            print(task)
     elif user_input == "4":
         print("Goodbye..")
     else:
