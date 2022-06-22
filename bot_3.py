3cfada64ca04933505924b6b61942b24056fa984
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2022-06-22 07:33:17 +0530

    Added user name in bot 3

--- bot_3.py
+++ bot_3.py
@@ -1,6 +1,9 @@
-# highly optimised to do bot
+# highly optimised to_do bot
+
 print("Hello This is BOT#3 a python powered Bot")
 print("I will manage all your todos..")
+print(" ")
+name = input("Enter your name : ")
 print(" ")
 print("You can enter 'exit' to exit and -r to check a task done")
 print(" ")
@@ -14,6 +17,7 @@ while task != "exit":
         rtask = input("Which task is done : ")
         if rtask in todo:
             todo.remove(rtask)
+            print(rtask, "marked as done")
             n -=1
         else:
             print(rtask, "not found in you todo list..")
@@ -21,7 +25,7 @@ while task != "exit":
         todo.append(task)
         n +=1
 print(" ")
-print("Asutosh you have "+str(n)+" tasks todo..")
+print(name, "you have "+str(n)+" tasks todo..")
 print(" ")
 for tasks in todo:
     print(tasks)
